c06d6fa7502e4fc53fa77c6d1ea09a1b6d69ebe6
Elina <elina@MacBook-Pro-Elina.local>
2025-10-02 22:40:34 +0300

    develop

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,14 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import './TodoInfo.scss';
+import todos from '../../api/todos.json';
+import { UserInfo } from '../UserInfo';
+
+export const TodoInfo = ({ todo }) => (
+  <article
+    className={
+      todo.completed === true ? 'TodoInfo TodoInfo--completed' : 'TodoInfo'
+    }
+  >
+    <h2 className="TodoInfo__title">{todos.title}</h2>
+    {todo.user && <UserInfo user={todo.user} />}
+  </article>
+);
